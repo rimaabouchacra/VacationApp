83171f9d5787c0c25a29a67907074fa957261db4
Reema224 <rima.abouchakra.gms@gmail.com>
2023-06-18 00:57:48 +0300

    added components, images, pages folder

--- vacation-app-frontend/src/App.js
+++ vacation-app-frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          {/* <Route path="/" element={<Homepage/>} />
+          <Route path="/signup" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/imagetest" element={<ImageTest/>} />
+          <Route path="/headingtest" element={<HeadingTest/>} />
+          <Route path="/linkbuttontest" element={<LinkButtonTest/>} /> */}
+      </Routes>  
+    </Router> 
   );
 }
 
--- vacation-app-server/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace vacation_app_server.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
