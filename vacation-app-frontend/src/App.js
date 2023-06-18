e5fe9afcc03b700df8e2c92a73a088e41274620d
Reema224 <rima.abouchakra.gms@gmail.com>
2023-06-18 21:00:18 +0300

    added navbar

--- vacation-app-frontend/src/App.js
+++ vacation-app-frontend/src/App.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import {Navigate} from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import Login from './pages/login_page';
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login/>} />
           {/* <Route path="/" element={<Homepage/>} />
           <Route path="/signup" element={<Register/>} />
           <Route path="/login" element={<Login/>} />
---
+++ vacation-app-frontend/src/pages/login_page/index.js
@@ -0,0 +1,13 @@
+import Navbar from "../../components/navbar";
+// import Loginn from "../../components/login";
+
+
+const Login=()=>{
+    return(
+        <div>
+          <Navbar activePage="login"/>
+          {/* <Loginn/> */}
+        </div>
+    )
+}
+export default Login;
