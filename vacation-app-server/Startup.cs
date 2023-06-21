b7554e4054be6e1395e706f128de57cdc1aac86c
Reema224 <rima.abouchakra.gms@gmail.com>
2023-06-21 23:59:10 +0300

    added controllers

--- vacation-app-server/Startup.cs
+++ vacation-app-server/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace vacation_app_server // Replace 'vacation_app_server' with your actual namespace
 {
@@ -16,20 +17,65 @@ namespace vacation_app_server // Replace 'vacation_app_server' with your actual 
             _configuration = configuration;
         }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Configure the DbContext for design-time operations
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+      public void ConfigureServices(IServiceCollection services)
+{
+    // Configure the DbContext for design-time operations
+    services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
 
-            // Add other services and dependencies
-            // ...
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    // Add JWT authentication
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
         {
-            // Configure middleware and other app settings
-            // ...
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "MyLocalAppIssuer",     
+                ValidAudience = "MyLocalAppAudience", 
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Ha6s0kJqDElpzsKj1smVkFPNrcD7ptnIZgcoe4ntKt1CAyuRzf0lZT2NdRRYYC36")) 
+            };
+        });
+
+}
+
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        // Configure appropriate error handling for production environment
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    // Enable HTTPS redirection
+    app.UseHttpsRedirection();
+
+    // Enable static file serving
+    app.UseStaticFiles();
+
+    // Enable routing
+    app.UseRouting();
+
+    // Enable authentication
+    app.UseAuthentication();
+
+    // Enable authorization
+    app.UseAuthorization();
+
+    // Configure the endpoints
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers(); // Assuming you have controllers for your API endpoints
+        // Add additional endpoint mappings as needed
+    });
         }
     }
 }
