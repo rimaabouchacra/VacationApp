9552b02f5caec64a72f2ecbc1ac271df87f6c113
Reema224 <rima.abouchakra.gms@gmail.com>
2023-06-22 18:06:10 +0300

    frontend: removed damaged

--- vacation-app-frontend/src/pages/add_vacation/index.js
+++
@@ -1,9 +0,0 @@
-import AddVacation from "../../components/add_vacation";
-const NewVacation=()=>{
-    return(
-        <div>
-          <AddVacation/>
-        </div>
-    )
-}
-export default NewVacation;
